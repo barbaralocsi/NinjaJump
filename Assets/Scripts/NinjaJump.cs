3030f0bd9dd953c2b5c8a7ffd9630a39144a1c51
Barbara Lócsi <locsi.barbara@gmail.com>
2019-04-02 14:45:20 +0200

    Add walls and jumping ninja ball.

---
+++ Assets/Scripts/NinjaJump.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaJump : MonoBehaviour
+{
+    [SerializeField] Vector3 jumpVector;
+
+    Rigidbody myRigidbody;
+
+    bool canJump = false;
+    float xVelocityAtEnter;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.velocity = jumpVector;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canJump)
+        {
+            Jump();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canJump = true;
+        xVelocityAtEnter = myRigidbody.velocity.x;
+    }
+
+    void Jump()
+    {
+        myRigidbody.velocity = new Vector3(jumpVector.x * -Mathf.Sign(xVelocityAtEnter), jumpVector.y, jumpVector.z);
+        canJump = false;
+    }
+
+
+}
