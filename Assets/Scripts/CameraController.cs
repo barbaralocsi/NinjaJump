9faee9bffadfb2374c5c2febbb5bdca3a2adb217
Barbara Lócsi <locsi.barbara@gmail.com>
2019-04-02 15:12:14 +0200

    Add CameraController. Camera follows GameObject with NinjaJump script attached.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float smoothTime = 0.3f;
+
+    GameObject player;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        player = FindObjectOfType<NinjaJump>().gameObject;
+    }
+
+    void Update()
+    {
+        // Matching y coordinate to player's y coordinate.
+        Vector3 targetPosition = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
